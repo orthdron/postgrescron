e5656ae01cc8b6dde438ef639dedaf5998abf63e
Deepak Kapoor <deepakdotpro@gmail.com>
2024-08-27 21:56:57 -0400

    Added dockerfiles

--- init-pg-cron.sh
+++ init-pg-cron.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Ensure pg_cron extension is created for the database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS pg_cron;
 EOSQL
